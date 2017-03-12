de1fbb246b1cac10dbd8a2d137345fa12a49be8c
Stouse49 <Stouse49@gmail.com>
2017-03-11 22:56:03 -0800

    Convert this seeder from Bitcoin to GoldCoin

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0xfd, 0xc2, 0xb4, 0xdd };
 
 CMessageHeader::CMessageHeader()
 {
